0fbf5e25e98d1f5091e31e3be959acaeea19aa2c
ArisGoi <aris.goi97@gmail.com>
2021-08-26 14:37:06 +0200

    add ottengo cognome + push

---
+++ main.js
@@ -0,0 +1,16 @@
+// 1. chiedi all’utente il cognome
+
+// 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+
+// 3. stampa la lista ordinata alfabeticamente
+
+// 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+
+
+// Array con i cognomi
+var listaCognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli",];
+
+// 1) Chiedi all'utente il suo cognome e lo inserisco in lista
+listaCognomi.push(prompt('Scrivi il tuo cognome'));
+
+console.log(listaCognomi)
