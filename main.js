3c249e192f3aeaab9312a0adb7c40763b998573b
ArisGoi <aris.goi97@gmail.com>
2021-08-26 15:01:07 +0200

    add posizione + stampa

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
-// 1. chiedi all’utente il cognome
-
-// 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
-
-// 3. stampa la lista ordinata alfabeticamente
-
-// 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
-
-
 // Array con i cognomi
 var listaCognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli",];
 
 // 1) Chiedi all'utente il suo cognome e lo inserisco in lista
-listaCognomi.push(prompt('Scrivi il tuo cognome'));
+var cognomeUtente = prompt('Scrivi il tuo cognome').toUpperCase();
+listaCognomi.push(cognomeUtente);
 
 // 2) Ordino alfabeticamente la lista
 listaCognomi.sort();
 
-console.log(listaCognomi)
+// 3) ottengo la posizione del cognome inserito in lista e stampo tutte le info
+console.log(listaCognomi);
+console.log("Il tuo cognome: " + cognomeUtente);
+console.log("Posizione del tuo cognome nella lista: " + (listaCognomi.indexOf(cognomeUtente) + 1))
